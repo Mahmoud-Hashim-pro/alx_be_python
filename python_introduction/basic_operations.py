7c0f8bca3568cbbb795a27679b76399d488ce41a
mahmoud <pro.mahmoud.h@gmail.com>
2025-05-21 21:53:15 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,14 @@
+number1 = 10
+number2 = 5
 
+sum_result = number1 + number2
+diff_result = number1 - number2
+prod_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {diff_result}")
+print(f"Multiplication of {number1} and {number2} is {prod_result}")
+
+
+
+Add basic_operations.py for Task 0
